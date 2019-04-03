a47903a519974753f64c963a2d52a4483e2fbf41
MikeRGarrity <mikergarrity@gmail.com>
2019-04-03 16:58:38 -0500

    setting up project, getting some dummy data to display

---
+++ src/todosData.js
@@ -0,0 +1,29 @@
+const todosData = [
+  {
+    id: 1,
+    text: "Feed the dog",
+    completed: true
+  },
+  {
+    id: 2,
+    text: "Grocery shopping",
+    completed: false
+  },
+  {
+    id: 3,
+    text: "Take out the trash",
+    completed: false
+  },
+  {
+    id: 4,
+    text: "Mow lawn",
+    completed: true
+  },
+  {
+    id: 5,
+    text: "Go on a run",
+    completed: false
+  }
+];
+
+export default todosData;
