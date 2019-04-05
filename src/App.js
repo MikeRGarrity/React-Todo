de7b33ff3b9a097e75170dee9275b66336caf433
MikeRGarrity <mikergarrity@gmail.com>
2019-04-04 19:27:41 -0500

    adding state to App class component

--- src/App.js
+++ src/App.js
@@ -4,11 +4,18 @@ import TodoItem from "./components/TodoItem";
 import todosData from "./todosData";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: todosData
+    };
+  }
+
   render() {
-    const todoComponents = todosData.map(item => (
+    const todoItems = this.state.todos.map(item => (
       <TodoItem key={item.id} item={item} />
     ));
-    return <div className="App">{todoComponents}</div>;
+    return <div className="App">{todoItems}</div>;
   }
 }
 
